675dfed17fc33a13e9cf4ee7bfc0aaecf67c4016
tamas516 <tamas516@gmail.com>
2022-05-13 20:22:15 +0200

    Movie-001: Basic Movie "database" and CI workflow

---
+++ progkorbead/src/main/java/hu/nye/progkor/movie/Main.java
@@ -0,0 +1,16 @@
+package hu.nye.progkor.movie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main class.
+ */
+@SpringBootApplication
+public class Main {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+
+}
